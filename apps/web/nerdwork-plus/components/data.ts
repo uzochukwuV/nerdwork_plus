59879ec553c499531134410552806471caae6da2
Ayomikun <ayoadeosun10@gmail.com>
2025-07-03 20:51:49 +0100

    feat(web): add link to tickets for comic con 2025

--- apps/web/nerdwork-plus/components/data.ts
+++ apps/web/nerdwork-plus/components/data.ts
@@ -1,13 +1,10 @@
-// import ComicCon from "@/assets/events/comic-con.jpg";
-// import TechSummit from "@/assets/events/tech-summit.jpg";
-// import ArtsFair from "@/assets/events/arts-fair.jpg";
-
 export const eventsData = [
   {
     id: 1,
-    src: "/assets/events/comic-con.jpg",
+    src: "/events/comic-con.jpg",
     alt: "Comic con",
-    date: "March 23, 2025",
+    link: "https://www.tketnation.com/nwcc25",
+    date: "September 6, 2025",
     title: "Comic Con 2025",
     subtitle:
       "Experience a day of creativity and connection at our annual community event! ",
@@ -16,7 +13,8 @@ export const eventsData = [
     id: 2,
     src: "/events/tech-summit.jpg",
     alt: "tech summit",
-    date: "May 215, 2025",
+    link: "",
+    date: "May 21, 2025",
     title: "Tech Innovation Summit 2025",
     subtitle:
       "Join industry leaders to explore the latest in technology and innovation.",
@@ -25,6 +23,7 @@ export const eventsData = [
     id: 3,
     src: "/events/arts-fair.jpg",
     alt: "arts fair",
+    link: "",
     date: "June 10, 2025",
     title: "Arts & Crafts Fair 2025",
     subtitle: "Showcase your talents and discover new art at our annual fair!",
