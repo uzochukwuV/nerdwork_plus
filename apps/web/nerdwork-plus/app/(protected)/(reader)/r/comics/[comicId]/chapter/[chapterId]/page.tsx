1595de87045e3d6e7ca330f0a915f2ff5c6b79bb
Ayomikun <ayoadeosun10@gmail.com>
2025-09-04 02:54:25 +0100

    feat: create comic reader route

---
+++ apps/web/nerdwork-plus/app/(protected)/(reader)/r/comics/[comicId]/chapter/[chapterId]/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ComicReader = () => {
+  const chapterPages = [
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+    "/events/anime-con.jpg",
+  ];
+  return <main className="w-full flex justify-center"></main>;
+};
+
+export default ComicReader;
