df30f91b510f4ed98ac8739ae9cf8dd1837f0ee3
Ayomikun <ayoadeosun10@gmail.com>
2025-09-05 10:28:20 +0100

    fix: fix isCustom error

--- apps/web/nerdwork-plus/lib/api/apiClient.ts
+++ apps/web/nerdwork-plus/lib/api/apiClient.ts
@@ -3,38 +3,38 @@ import axios from "axios";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-async function axiosGet(url: string, isCustom?: boolean) {
+async function axiosGet(url: string) {
   const config = {
     headers: {
       "Content-Type": "application/json",
     },
   };
 
-  const fullUrl = isCustom ? url : `${apiUrl}${url}`;
+  const fullUrl = `${apiUrl}${url}`;
 
   const response = await axios.get(fullUrl, config);
   return response;
 }
 
-async function axiosPost<T>(url: string, body: T, isCustom?: boolean) {
+async function axiosPost<T>(url: string, body: T) {
   const config = {
     headers: {
       "Content-Type": "application/json",
     },
   };
-  const fullUrl = isCustom ? url : `${apiUrl}${url}`;
+  const fullUrl = `${apiUrl}${url}`;
 
   const response = await axios.post(fullUrl, body, config);
   return response;
 }
 
-async function axiosImagePost<T>(url: string, body: T, isCustom?: boolean) {
+async function axiosImagePost<T>(url: string, body: T) {
   const config = {
     headers: {
       "Content-Type": "multipart/formData",
     },
   };
-  const fullUrl = isCustom ? url : `${apiUrl}${url}`;
+  const fullUrl = `${apiUrl}${url}`;
 
   const response = await axios.post(fullUrl, body, config);
   return response;
